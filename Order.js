b7c56eb7f1fde0e387ffa25401d3caf9e64f1057
mhk98 <mohsinkabirseo@gmail.com>
2024-02-27 23:45:27 +0600

    added api

--- Order.js
+++ Order.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Cart from './src/Cart';
-import useProducts from './src/hooks/useProducts';
+import React from "react";
+import Cart from "./src/Cart";
+import useProducts from "./src/hooks/useProducts";
 
 const Order = () => {
-    const [products, setProducts] = useProducts()
+  const [products, setProducts] = useProducts();
 
-    return (
-        <div>
-            {
-                products?.map(product => <Cart key={product.id} product={product}></Cart>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      {products?.map((product) => (
+        <Cart key={product.id} product={product}></Cart>
+      ))}
+    </div>
+  );
 };
 
 export default Order;
--- src/feature/cart/cart.js
+++ src/feature/cart/cart.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productCartApi = createApi({
   reducerPath: "productCartApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/",
+    baseUrl: "https://pos-backend-lgrj.onrender.com/api/v1/",
   }),
 
   tagTypes: ["carts"], // Define the tag type
